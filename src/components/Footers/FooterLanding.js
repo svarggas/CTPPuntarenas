60284916e442a60c31fef1dc236ed50ad3b01ec5
sebastianvargasr <sebastian.vargas4@ulatina.net>
2021-08-07 13:28:18 -0600

    Footer adjustment

--- src/components/Footers/FooterLanding.js
+++ src/components/Footers/FooterLanding.js
@@ -74,7 +74,7 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-gray-600 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} by XXX.
+                Copyright © {new Date().getFullYear()} by KKS Development team.
               </div>
             </div>
           </div>
